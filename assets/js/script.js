b7ed6edbcee00a3ff1657aa5ebe2c7a1ada4273d
Pasha Fomin <pashafomin@Pashas-MacBook-Pro.local>
2022-11-13 20:23:31 -0800

    weather cards functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // elements
 var apiKey = '9cfe7036b90b3a13af1a88f6bf534b32'
 
-var searchBtn = document.querySelector('.searchBtn')
+var searchBtn = document.getElementById('searchBtn1')
 
 // main info box
 var fullBox = document.querySelector('.returnedData')
 var mainBox = document.querySelector('.currentDay')
 // 5 day blocks
+var blocksContainer = document.querySelector('.dayBlocks')
+
 var weatherBox1 = document.getElementById('weatherBox1')
 var weatherBox2 = document.getElementById('weatherBox2')
 var weatherBox3 = document.getElementById('weatherBox3')
@@ -22,7 +24,18 @@ var newP1 = document.createElement('p')
 var newP2 = document.createElement('p')
 var newP3 = document.createElement('p')
 
+
+
 var currentDate = moment().format('MMMM Do YYYY')
+var day1 = moment().add(1, 'days').format('L')
+var day2 = moment().add(2, 'days').format('L')
+var day3 = moment().add(3, 'days').format('L')
+var day4 = moment().add(4, 'days').format('L')
+var day5 = moment().add(5, 'days').format('L')
+
+// search history
+
+
 
 
 
@@ -30,6 +43,7 @@ var currentDate = moment().format('MMMM Do YYYY')
 // functions
 function getCurrentWeather(e) {
     e.preventDefault()
+
 
     fullBox.style.display = 'block'
 
@@ -53,8 +67,8 @@ function getCurrentWeather(e) {
             var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png"
             newIcon.setAttribute('src', iconUrl)
 
-            var iconDesription = data.weather[0].description
-            newIcon.setAttribute('alt', iconDesription)
+            var iconDescription = data.weather[0].description
+            newIcon.setAttribute('alt', iconDescription)
 
             var temp = Math.floor(((data.main.temp - 273.15) * 1.8 + 32))
             newP1.textContent = 'Temperature: ' + temp + ' °F'
@@ -73,6 +87,8 @@ function getCurrentWeather(e) {
             mainBox.appendChild(newP2)
             mainBox.appendChild(newP3)
         })
+
+        
 }
 
 
@@ -83,6 +99,7 @@ function getForecast(e) {
     e.preventDefault()
 
     var inputValue = document.getElementById('searchInput').value
+    
 
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}`
 
@@ -94,17 +111,66 @@ function getForecast(e) {
             console.log(data)
 
             // box 1 append
-            var name = data.city.name
-            
-
-
-
-            for (var i = 0; i < data.length; i++) {
+            for (var i = 0; i < data.list.length; i += 8) {
+                var dt = data.list[i].dt_txt
+                var dt1 = dt.split('')
+                var dateArr = dt1.splice(0, 10)
+                var date = dateArr.join('')
                 
-            }
 
-           
+                var icon = data.list[i].weather[0].icon
+                var iconDescription = data.list[i].weather[0].description
+                var temp = Math.floor(((data.list[i].main.temp - 273.15) * 1.8 + 32))
+                var wind = data.list[i].wind.speed
+                var conversion = Math.ceil((wind * 2.237) * 100) / 100
+                var humidity = data.list[i].main.humidity
+                
+                var newDiv = document.createElement('div')
+                newDiv.classList.add('weatherBox')
+
+                var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png"
+
+                var newHead = document.createElement('h5')
+                var newIcon = document.createElement('img')
+                var newP1 = document.createElement('p')
+                var newP2 = document.createElement('p')
+                var newP3 = document.createElement('p')
+                
+                newHead.textContent = date
+                newHead.setAttribute('style', 'font-weight: bold; text-decoration: underline;')
+                newIcon.setAttribute('src', iconUrl)
+                newIcon.setAttribute('alt', iconDescription)
+                newP1.textContent = 'Temp: ' + temp + ' °F'
+                newP2.textContent = 'Wind: ' + conversion + ' MPH'
+                newP3.textContent = 'Humidity: ' + humidity + '%'
+
+                newDiv.appendChild(newHead)
+                newDiv.appendChild(newIcon)
+                newDiv.appendChild(newP1)
+                newDiv.appendChild(newP2)
+                newDiv.appendChild(newP3)
+                
+                
+                blocksContainer.appendChild(newDiv)
+
+                searchBtn.disabled = true
+            }     
         })
+
+    var button = document.createElement('button')
+
+    button.classList.add('searchBtn')
+    button.textContent = 'New Search'
+    
+    fullBox.appendChild(button)
+
+    button.addEventListener('click', function(e) {
+        e.preventDefault
+        window.location.reload();
+
+    })
+    
+
 }
 
 
